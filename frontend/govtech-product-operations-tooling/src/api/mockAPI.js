689de14bc68cb5e157cdf0f75d6f99533596e4a6
YAP SAI WEI MARCUS <yap.marcus@yahoo.com.sg>
2025-02-16 19:08:28 +0800

    mock data and API

---
+++ frontend/govtech-product-operations-tooling/src/api/mockAPI.js
@@ -0,0 +1,26 @@
+import students from "@data/students.json";
+import teachers from "@data/teachers.json";
+
+export const fetchStudents = async () => {
+	return students;
+};
+
+export const fetchTeachers = async () => {
+	return teachers;
+};
+
+export const fetchStudentById = async (id) => {
+	return students.find((student) => student.id === id);
+};
+
+export const fetchTeacherById = async (id) => {
+	return teachers.find((teacher) => teacher.id === id);
+};
+
+export const fetchStudentsByTeacherId = async (teacherId) => {
+	return students.filter((student) => student.teacherId === teacherId);
+};
+
+export const fetchTeachersByStudentId = async (studentId) => {
+	return teachers.filter((teacher) => teacher.studentIds.includes(studentId));
+};
